2c5ba1ae7ea72ba09f75133f6c3a460240bb5cfb
안주홍 <anjuhong@anjuhongs-MacBook-Pro.local>
2023-07-10 19:32:25 +0900

    hotfix data

--- js/signin_first.js
+++ js/signin_first.js
@@ -1,13 +1,12 @@
 async function signin() {
     let id = document.getElementById('id').value;
     let password = document.getElementById('password').value;
-    let body = {
-        "user_id": id,
-        "password" : password
-    }
 
-    let url = "http://210.109.62.129:8080/sign-in?user_id=" + id + "&password=" + password
+    let url = "http://127.0.0.1:8080/sign-in?user_id=" + id + "&password=" + password
 
     const aa = await fetch(url)
-    console.log(aa)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    })
 }
